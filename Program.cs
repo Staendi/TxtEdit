6825a9241b7c71437d693d62699258a1a27bc747
nicos <nicos@PCVONNICO>
2021-08-29 18:21:30 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace TxtEdit
+{
+    class Program
+    {
+        /// <summary>
+        /// Ein Programm, welches eine Txt lesen und schreiben kann
+        /// </summary>
+        static void Main()
+        {
+            //Pfad zur Txt zum auslesen
+            StreamReader streamReader = new StreamReader("D:/Coding/VSProjects/TxtEdit/Txts/Read.txt");
+            //Pfad zur Txt zum schreiben                              //true, damit der Text angefügt und nicht ersetzt wird
+            StreamWriter streamWriter = new StreamWriter("D:/Coding/VSProjects/TxtEdit/Txts/Write.txt", true);
+
+            //Inhalt der Txt in Console ausgeben lassen
+            Console.WriteLine(streamReader.ReadToEnd());
+            streamReader.Close();
+            //Inhalt der Txt schreiben
+            //Das \n um in die nächste Zeile zu springen
+            streamWriter.WriteLine("\nHallo");
+            streamWriter.Close();
+
+
+            
+
+        }
+    }
+}
